ce48cfacbc97d10c7bd342ddeca87a7194ba16d9
parma1111 <parmarambuldak800@gmail.com>
2022-05-19 04:39:34 -0700

    Inheritance

---
+++ index.js
@@ -0,0 +1,11 @@
+const fourwheeler = {
+    wheels : 4,
+    brand : 'tata',
+    seater : 4,
+    wheelbrand : 'appollo',
+
+}
+
+const c1 = Object.create(fourwheeler)
+console.log("c1:", c1);
+
---
+++ index1.js
@@ -0,0 +1,6 @@
+function fourwheeler(n, u, p, k) {
+    (this.brand = n), (this.wheels = u), (this.seater = p), (this.wheelbrand = k);
+}
+
+let c2 = new fourwheeler("tata", 4, 4, "appollo");
+console.log("c2:", c2);
